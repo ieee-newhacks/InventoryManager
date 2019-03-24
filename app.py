1e270606f247be5ad615071f3f7a7af505deeb41
Daniel <dannykim200@gmail.com>
2019-03-23 21:26:03 -0400

    changing flask files

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, Markup, request, redirect
-from config import col, col_results
-import requests
-import random
+from flask import Flask, render_template, Markup, request, redirect, url_for # For flask
+from bson import ObjectId # for ObjectId to work
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+title = "SYDE Order: An Inventory Manager for retailers to determine what's hot and what's not."
+heading = "SYDE Order"
 
-@app.route('/', method=['GET'])
+client = MongoClient("mongodb+srv://sydemans:sydemans123@cluster0-qcrqx.mongodb.net/test?retryWrites=true")
+db = client.Inventory
+clothes = db.tshirt
+
+
+@app.route('/', methods=['GET'])
 def home(): 
-    return render_template('/index.html')
+    if request.method=='GET':
+        return render_template('/index.html')
+    else:
+        return render_template('/index.html')
--- inventory.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://sydemans:sydemans123@cluster0-qcrqx.mongodb.net/test?retryWrites=true")
-
-print(client.mflix)
